0f79175567200320f0dc41ff41faf9e0d20a7b3a
chensov <senche.dev@gmail.com>
2023-10-20 19:53:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# My CV NPMmmmmm...
+
+`arsen-cv` is a simple command-line tool for displaying my resume. It's fun to give it a go.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Uninstallation](#uninstallation)
+
+## Installation
+
+To use `arsen-cv`, you can install it globally using npm. Open your terminal and run:
+
+```bash
+npm install -g arsen-cv
+```
+
+Make sure you have Node.js and npm installed on your system before proceeding.
+
+## Usage
+
+After installing `arsen-cv`, you can run it from the command line with the following command:
+
+```bash
+arsen-cv
+```
+
+This will display an ASCII representation of my CV.
+
+## Uninstallation
+
+If you want to uninstall `arsen-cv` from your system, you can do so using npm. Open your terminal and run:
+
+```bash
+npm uninstall -g arsen-cv
+```
+
+This will remove the `arsen-cv` package from your global npm packages.
+
+Please note that this action will also delete the customized `cv.sh` script if you have made any changes to it.
---
+++ cv.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+source .env
+
+
+
+echo "$NAME"
+echo
+
+
+
+echo -e "📍 $LOCATION"
+echo
+
+
+WIDTH=40
+
+echo -e "\033[34m"
+echo "Phone: $PHONE_NUMBER"
+echo "----------------------------------------"
+echo "Email: $EMAIL"
+echo "----------------------------------------"
+echo "Website: $WEBSITE"
+echo "----------------------------------------"
+echo "GitHub: $GITHUB"
+echo -e "----------------------------------------\033[0m\n"
+
+
+echo -n -e "\033[1m\\\\"
+echo -e " Profile\033[0m\n"
+
+
+echo -e "$PROFILE"
+echo
+
+echo -e "\033[32m+------------+  +-------+  +-----+  +-----+  +-----+  +------+"
+echo -e "| JavaScript |  | HTML  |  | CSSS|  | SASS|  | PHP |  | MySQL|"
+echo -e "+------------+  +-------+  +-----+  +-----+  +-----+  +------+\033[0m"
+
+
+echo -e "\033[1m\\ Education and Qualifications\033[0m\n"
+echo
+
+echo "2022       PRINCE2 Foundation Certification"
+echo
+
+echo "2017–2021  University of East London    BSc in Economics"
+echo "           London, United Kingdom        2:1"
+echo
+
+
